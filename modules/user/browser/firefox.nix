6773c2437085d39bd85dae7c86dd381f5020e166
William Dreyer <w.h.dreyer@icloud.com>
2025-03-16 22:23:47 -0400

    initial commit

--- modules/user/browser/firefox.nix
+++ modules/user/browser/firefox.nix
@@ -1,8 +1,14 @@
-{ config, pkgs, userSettings, ... }:
+{ config, pkgs, inputs, userSettings, systemSettings, ... }:
 
 {
-
-  programs.firefox.enable = true;
-
+  programs.firefox = {
+    enable = true;
+    profiles.${userSettings.username} = {
+      extensions = [
+        inputs.firefox-addons.packages.${systemSettings.system}.bitwarden
+        inputs.firefox-addons.packages.${systemSettings.system}.ublock-origin
+      ];
+    };
+  };
 }
 
